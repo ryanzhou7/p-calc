e4524885b8ba7663e6ee6ed00f4ac0ebed1afea9
Ryan Zhou <ryanzhou7@gmail.com>
2020-05-17 09:04:17 -0700

    Make same dom.js

--- src/utils/DOM.js
+++ src/utils/DOM.js
@@ -2,8 +2,10 @@ async function handleImageInputChange(event, setImage) {
   if (event.target.files && event.target.files.item(0)) {
     const imageSource = URL.createObjectURL(event.target.files[0]);
     const image = new Image();
-    image.onload = () => setImage(image);
     image.src = imageSource;
+    image.onload = () => {
+      setImage(image);
+    };
   }
 }
 
